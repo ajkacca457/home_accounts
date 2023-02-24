1f95be840efd12a6885f479bda3243f49fd38a5a
Avijit Karmaker <ajkacca@gmail.com>
2023-02-24 15:12:36 +0200

    update front page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='bg-red-200 px-2 py-2'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className='bg-red-300 px-2 py-2'>Home Accounts</p>
     </div>
   );
 }
