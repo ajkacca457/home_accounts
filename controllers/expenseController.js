711acb5604a4034039eafe435e5af2337b2cce54
Avijit Karmaker <ajkacca@gmail.com>
2023-03-05 02:02:26 +0200

    update expense controller,model

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1,11 +1,18 @@
 import Expense from "../models/Expense.js"
+import AsyncHandler from "../middlewares/AsyncHandler.js";
+import CustomError from "../utils/CustomError.js";
+import { StatusCodes } from "http-status-codes";
 // for all expenses
 
-export const getExpenses=(req,res,next)=>{
-    res.status(200).json({
-        message:"will return all the expenses"
+export const getExpenses=AsyncHandler(async (req,res,next)=>{
+    const expenses= await Expense.find();
+    if(!expenses) {
+        return next(new CustomError("Transactions are not available", StatusCodes.NOT_FOUND))
+    }
+    res.status(StatusCodes.OK).json({
+        data:expenses
     })
-}
+})
 
 // for expense statistics
 
@@ -16,30 +23,61 @@ export const getExpenseStats=(req,res,next)=>{
 }
 
 //for single expense 
- export const getSingleExpense=(req,res,next)=>{
+export const getSingleExpense=AsyncHandler(async (req,res,next)=>{
+    const {id}= req.params;
+
+    const expense= await Expense.findById(id);
+
+    if(!expense) {
+        return next( new CustomError(`Transaction with ID ${id} doesnt exists`, StatusCodes.NOT_FOUND));
+    }
+
     res.status(200).json({
-        message:"will get a single expense"
+        data:expense
     })
-}
+})
 
 //  for create single expense 
-export const createExpense=(req,res,next)=>{
-    res.status(200).json({
-        message:"will create single expense"
-    })
-}
+export const createExpense=AsyncHandler(async (req,res,next)=>{
+    const {title,information,amount,category}= req.body;
 
+    const expense= await Expense.create({
+        title,information,amount,category
+    });
+
+    if(!expense) {
+        return next(CustomError("Income cant be created", StatusCodes.BAD_REQUEST))
+    }
+
+    res.status(StatusCodes.CREATED).json({
+        data: expense
+    })
+})
 
 //  for updating single expense 
-export const updateExpense=(req,res,next)=>{
-    res.status(200).json({
-        message:"will update single expense"
+export const updateExpense=AsyncHandler(async (req,res,next)=>{
+    const expense= await Expense.findByIdAndUpdate(req.params.id, req.body, {
+        new:true,
+        runValidators:true
     })
-}
+
+    if(!expense) {
+        return next(new CustomError(`${req.params.id} transaction cant be updated`,StatusCodes.BAD_REQUEST))
+    }
+
+    res.status(200).json({
+        data:expense,
+        message:"Transaction updated succesfully"
+    })
+})
 
 //  for deleting single expense 
-export const deleteExpense=(req,res,next)=>{
+export const deleteExpense=AsyncHandler(async (req,res,next)=>{
+    const expense= await Expense.findByIdAndDelete(req.params.id);
+    if(!expense) {
+        return next(new CustomError(`${req.params.id} transaction cant be deleted`,StatusCodes.BAD_REQUEST))
+    }
     res.status(200).json({
-        message:"will delete single expense"
+        message:`${req.params.id} transaction deleted` 
     })
-}
+})
--- models/Expense.js
+++ models/Expense.js
@@ -2,13 +2,31 @@ import mongoose from "mongoose";
 
 
 const ExpenseSchema= new mongoose.Schema({
-
-title: {
+    title: {
+        type:String,
+        required: [true,"transaction title is required"],
+        maxLength: [100, "title can't be more than 100 charachter"],
+        trim:true
+    },
+    information: {
+        type:String,
+        require:[true,"transaction information is required"],
+        minLength:[10,"information cant be shorter than 10 charachter"],
+        maxLength:[100,"information cant be longer than 100 charachter"],
+    },
+    amount: {
+        type:Number,
+        required: [true, "transaction amount is required"],
+    },
+    category: {
     type:String,
-    required: [true,"transaction title is required"],
-    maxLength: [100, "title can't be more than 100 charachter"],
-    trim:true
-}
+    required:[true, "category is required"],
+    enum: ["Groceries","Rent", "Insurance", "Entertainment", "Education","Health","Miscellaneous"]
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
 
 })
 
