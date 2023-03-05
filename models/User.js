9769bef68210c135defdd1cd244c3a3f0d334d19
Avijit Karmaker <ajkacca@gmail.com>
2023-03-05 13:56:53 +0200

    update User Model

--- models/User.js
+++ models/User.js
@@ -1,13 +1,39 @@
 import mongoose from "mongoose";
 
 const UserSchema= new mongoose.Schema({
-    name:{
+    firstname:{
         type:String,
-        required:[true,"Name of user is required"],
-        maxLength:[50,"Name cant be longer than 50 charachter"],
-        minLength:[10,"Full name is required"],
+        required:[true,"first name of user is required"],
+        minLength:[3,"first name cant be shorter than 3 charachter"],
+        maxLength:[20,"first name cant be longer than 20 charachter"],
         trim:true
+    },
+    lastname:{
+        type:String,
+        required:[true,"last name of user is required"],
+        minLength:[3,"last name cant be shorter than 3 charachter"],
+        maxLength:[20,"last name cant be longer than 20 charachter"],
+        trim:true
+    },
+    email: {
+        type:String,
+        unique:true,
+        required:[true, "email is required for user"]
+    },
+    password:{
+        type:String,
+        required:[true, "password is required for user"],
+        trim:true,
+        minLength:6,
+        maxLength:25
+    },
+    location: {
+        type:String,
+        trim:true,
+        maxLength:25,
+        default:"My City"
     }
+
 })
 
 
