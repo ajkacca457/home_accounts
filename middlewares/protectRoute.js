fb5d605c33841021c78424bc0b85fb8afd505b6d
Avijit Karmaker <ajkacca@gmail.com>
2023-03-11 14:24:41 +0200

    updated protect auth

---
+++ middlewares/protectRoute.js
@@ -0,0 +1,29 @@
+import AsyncHandler from "./AsyncHandler.js";
+import CustomError from "../utils/CustomError.js";
+import { StatusCodes } from "http-status-codes";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const protectRoute=AsyncHandler(async(req,res,next)=>{
+    let token;
+
+    if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        token = req.headers.authorization.split(" ")[1];
+    }
+
+    if(!token) {
+        return next(new CustomError("not authorized to access the data",StatusCodes.UNAUTHORIZED))
+    }
+
+    try {
+        const decodedId= jwt.verify(token,process.env.JWT_SECRET);
+        req.user= {userId:decodedId};
+        next();
+    } catch (error) {
+        return next(new CustomError(error.message,StatusCodes.UNAUTHORIZED))
+    }
+
+})
+
+
+export default protectRoute;
--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { getExpenses, getSingleExpense,getExpenseStats,createExpense,updateExpense,deleteExpense } from "../controllers/expenseController.js";
+import protectRoute from "../middlewares/protectRoute.js";
+
 
 const router= express();
 
-router.route("/").get(getExpenses).post(createExpense);
+router.route("/").get(getExpenses).post(protectRoute,createExpense);
 router.route("/expense-stats").get(getExpenseStats);
 router.route("/:id").get(getSingleExpense).patch(updateExpense).delete(deleteExpense);
 
