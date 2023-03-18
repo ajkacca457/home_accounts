39245099c86dffecf643175edebc4acf696b49f6
Avijit Karmaker <ajkacca@gmail.com>
2023-03-18 19:26:17 +0200

    update auth page with login and register forms

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -9,7 +9,8 @@ const initailValues= {
     firstname:"",
     lastname:"",
     email:"",
-    password:""
+    password:"",
+    isRegistered:false
 };
 
 const [values,setValues]= useState(initailValues);
@@ -20,6 +21,11 @@ const handleChange=(e)=>{
     console.log(e.target.value);
 }
 
+const toggleForm=(e)=>{
+    e.preventDefault();
+    setValues({...values,isRegistered:!values.isRegistered})
+}
+
   return (
         <div class="w-3/6 mx-auto">
             <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -27,29 +33,22 @@ const handleChange=(e)=>{
                     <img src={AccountLogo} alt="logo" className='w-[300px] h-[60px]'/>
                 </div>
 
-                <h1 className='font-heading text-center my-6'>Register</h1>
-
-                <FormRow type="text" name="firstname" value={values.firstname} labelText="Firstname" placeholderText="Enter your firstname" handleChange={handleChange}/>
-
-                <div class="mb-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                        Username
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
-                </div>
-    
-                <div class="mb-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                        Password
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
-                </div>
+                <h1 className='font-heading text-center my-6'>{!values.isRegistered?"Register":"Login"}</h1>
+                {!values.isRegistered && <FormRow type="text" name="firstname" value={values.firstname} labelText="Firstname" placeholderText="Enter your first name" handleChange={handleChange}/>}
+                {!values.isRegistered && <FormRow type="text" name="lastname" value={values.lastname} labelText="Lastname" placeholderText="Enter your last name" handleChange={handleChange}/>}
+                <FormRow type="email" name="email" value={values.email} labelText="Email" placeholderText="Enter email address" handleChange={handleChange}/>
+                <FormRow type="password" name="password" value={values.password} labelText="Password" placeholderText="Enter password" handleChange={handleChange}/>
     
                 <div class="flex items-center justify-between">
                     <button class="btn-primary" type="button">
-                        Sign In
+                        {!values.isRegistered?"Submit":"Sign In"}
                     </button>
                 </div>
+                
+                <div className='text-right text-cyan-700'>
+                    {!values.isRegistered?<p>Already a member? Please <button className='btn-underline' onClick={toggleForm}>Login</button> with your credintials.</p>:<p>Dont have an account? <button className='btn-underline' onClick={toggleForm}>Register</button> first to use the app.</p>}
+                </div>
+
             </form>
         </div>
   )
