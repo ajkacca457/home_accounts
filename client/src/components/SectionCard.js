11c117a20abcddda8dc9a14b136fe6d20144f7f1
Avijit Karmaker <ajkacca@gmail.com>
2023-03-18 13:57:52 +0200

    update section card component

--- client/src/components/SectionCard.js
+++ client/src/components/SectionCard.js
@@ -1,15 +1,14 @@
 import React from 'react'
 
-const SectionCard = (props) => {
-    console.log(props);
+const SectionCard = ({cardImage,left,info,title}) => {
 
   return (
     <div className="image-card grid grid-cols-12 items-center">
-        <div className='col-span-6'>
-            <h1 className='font-heading'>Lorem ipsum dolor</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi dolor unde suscipit tempore eveniet maxime! Asperiores eligendi molestias sit libero?</p>
+        <div className={`col-span-6 ${left?"order-2":""}`}>
+            <h1 className='font-heading'>{title.toUpperCase()}</h1>
+            <p>{info}</p>
         </div>
-        <img src="" alt="" className='col-span-6'/>
+        <img src={cardImage} alt="" className='col-span-6'/>
     </div>  )
 }
 
---
+++ client/src/utilites/cardData.js
@@ -0,0 +1,8 @@
+import Finace from "../assets/finance.jpg";
+
+export const cardDetails = {
+    cardImage:Finace,
+    title:"Track your finances easily",
+    left:false,
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi dolor unde suscipit tempore eveniet maxime! Asperiores eligendi molestias sit libero?"
+}
