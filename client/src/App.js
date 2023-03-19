ca464ef247587f58c523924311e919371bd18763
Avijit karmaker <ajkacca@gmail.com>
2023-03-20 00:57:42 +0200

    add new pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Auth from './pages/Auth';
 import Stats from './pages/Stats';
 import IncomeList from './pages/IncomeList';
 import ExpenseList from './pages/ExpenseList';
+import Error from './pages/Error';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
@@ -11,10 +12,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' index element={<Home/>}/>
-          <Route path='/auth' element={<Auth/>}/>
           <Route path='/stats' element={<Stats/>}/>
           <Route path='/incomes' element={<IncomeList/>}/>
           <Route path='/expenses' element={<ExpenseList/>}/>
+          <Route path='/auth' element={<Auth/>}/>
+          <Route path='/*' element={<Error/>}/>
         </Routes>
       </BrowserRouter>
     </div>
