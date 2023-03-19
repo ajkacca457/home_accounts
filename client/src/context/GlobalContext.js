03576a8db23f91243f33596ecf20257aa7b994cd
Avijit karmaker <ajkacca@gmail.com>
2023-03-19 22:53:54 +0200

    add reducers,actions

--- client/src/context/GlobalContext.js
+++ client/src/context/GlobalContext.js
@@ -1,18 +1,22 @@
-import { createContext, useContext } from "react";
-
+import { createContext, useContext, useReducer } from "react";
+import GlobalReducer from "./reducers/GlobalReducer";
 
 const GlobalContext= createContext();
 
 const initialState= {
-    name:"",
-    address:"",
-    demo:""
+    isLoading:false,
+    showAlert:false,
+    alertClasses:"",
+    alertText:"",
 }
 
 const GlobalContextProvider=({children})=>{
 
+    const [state,dispatch]= useReducer(GlobalReducer,initialState)
+
+
     return (
-        <GlobalContext.Provider value={{...initialState}}>
+        <GlobalContext.Provider value={{...state}}>
             {children}
         </GlobalContext.Provider>
     )
---
+++ client/src/context/reducers/GlobalReducer.js
@@ -0,0 +1,5 @@
+const GlobalReducer=(state,action)=>{
+    return state;    
+}
+
+export default GlobalReducer;
