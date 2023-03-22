f26f73f701a268d62aed13a17c8f8181272daca6
Avijit Karmaker <ajkacca@gmail.com>
2023-03-22 21:56:00 +0200

    update navigation

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { useGlobalContext } from '../context/GlobalContext'
 import { links } from '../utilites/linkData'
+import {RiLogoutBoxRFill} from  "react-icons/ri"
 
 const Navigation = () => {
 
@@ -12,7 +13,7 @@ const {logOutUser}= useGlobalContext();
         {links.map((item)=>{
             return <NavLink to={item.path} key={item.id} end className={({isActive})=>isActive?"flex items-center bg-cyan-600 text-white px-2 py-1 rounded gap-[5px] active":"flex items-center gap-[5px]"}>{item.icon}{item.text}</NavLink>
         })}
-        <button onClick={logOutUser}>Logout</button>
+        <button onClick={logOutUser} className="flex items-center gap-[5px] bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600"> <RiLogoutBoxRFill/><span>Logout</span></button>
     </div>
   )
 }
