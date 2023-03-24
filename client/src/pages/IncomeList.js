7194d9bfbcc91bed1f62e2b32a2c9603fbf50732
Avijit karmaker <ajkacca@gmail.com>
2023-03-24 15:34:07 +0200

    update incomesList page

--- client/src/pages/IncomeList.js
+++ client/src/pages/IncomeList.js
@@ -1,12 +1,23 @@
 import React, { useEffect } from 'react'
 import { useGlobalContext } from '../context/GlobalContext'
+import Loading from "../components/Loading";
 
 const IncomeList = () => {
-  const {getIncomes}= useGlobalContext();
+  const {isLoading,incomes,getIncomes}= useGlobalContext();
 
   useEffect(()=>{
     getIncomes();
   },[]);
+
+  if(isLoading) {
+    return <Loading/>
+  }
+  
+  if(incomes.length===0) {
+    return  <div className='bg-white my-8 p-4 text-center'>
+              <h1>No transactions available..</h1>
+            </div>
+  }
 
   return (
     <div>IncomeList</div>
