aacf5bbcb71e91e328e8899931777d927c02fefc
Avijit Karmaker <ajkacca@gmail.com>
2023-03-27 00:13:16 +0300

    update form redirection

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import FormRow from './FormRow';
 import { incomeCategories,expenseCategories } from '../utilites/linkData';
 import { useGlobalContext } from '../context/GlobalContext';
+import {useNavigate} from "react-router-dom";
 
 const AddForm = () => {
 
@@ -17,15 +18,16 @@ const AddForm = () => {
 
     const [formvalues,setFormValues]= useState(initialValues);
 
-    const {addTransaction}= useGlobalContext();
+    const {addTransaction,displayAlert}= useGlobalContext();
 
+    const navigate= useNavigate(); 
 
 const handleSubmit=(e)=>{
     e.preventDefault();
     const {title,information,amount,category,status,type}= formvalues;
 
     if(!title||!information||!amount||!category||!status) {
-        alert("please fill all the fields");
+        displayAlert("bg-red-400 text-white","please fill all the fields");
         return;
     }
 
@@ -38,9 +40,15 @@ const handleSubmit=(e)=>{
     }
 
     if(type==="income") {
-        addTransaction("/incomes",transaction)
+        addTransaction("/incomes",transaction);
+        setTimeout(()=>{
+            navigate("/dashboard/incomes")
+        },1000)
     } else {
         addTransaction("/expenses",transaction)
+        setTimeout(()=>{
+            navigate("/dashboard/expenses")
+        },1000)
     }
 
 }
@@ -71,6 +79,7 @@ const handleChange=(e)=>{
              </select>
              <label htmlFor="status" className="block text-gray-700 text-sm font-bold mt-6 mb-2">Status</label>
             <select name="status" id="status" value={formvalues.status} onChange={handleChange} className="w-full py-2 px-2 border-2 rounded ">
+                {formvalues.status===""?<option hidden>Select Status..</option>:""}
                 <option value="received">received</option>
                 <option value="incoming">incoming</option> 
             </select>
@@ -85,6 +94,7 @@ const handleChange=(e)=>{
              </select>
              <label htmlFor="status" className="block text-gray-700 text-sm font-bold mt-6 mb-2">Status</label>
             <select name="status" id="status" value={formvalues.status} onChange={handleChange} className="w-full py-2 px-2 border-2 rounded ">
+                {formvalues.status===""?<option hidden>Select Status..</option>:""}
                 <option value="paid">paid</option>
                 <option value="due">due</option> 
             </select>
