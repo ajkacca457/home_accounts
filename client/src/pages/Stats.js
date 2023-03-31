e8d4cc287ff3ae3b2db2d07a9b00616357895486
Avijit Karmaker <ajkacca@gmail.com>
2023-03-31 15:27:19 +0300

    update design

--- client/src/pages/Stats.js
+++ client/src/pages/Stats.js
@@ -22,29 +22,29 @@ const Stats = () => {
   return (
     <div>
         <div className="income-stats grid grid-cols-12 gap-x-[10px]">
-            <div className='col-span-full flex justify-between px-4'>
+            <div className='col-span-full flex justify-between px-4 bg-purple-600 text-white my-4 py-2 rounded'>
               <h1>Income category statistics</h1>
-              <h1>{IncomeAmount}</h1>
+              <h1>Total : {IncomeAmount}</h1>
             </div>
             <div className='col-span-full grid grid-cols-12 gap-x-[10px]'>
             {incomeCategoryStats && Object.values(incomeCategoryStats).map((item,index)=>{
                 return (
-                  <StatCard {...item} bgclass={"bg-green-300"} key={index}/>
+                  <StatCard {...item} bgclass={"bg-green-400"} key={index}/>
                 )
             })}
             </div>
 
             <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700 col-span-full" />
 
-            <div className='col-span-full flex justify-between px-4'>
+            <div className='col-span-full flex justify-between px-4 bg-purple-600 text-white my-4 py-2 rounded'>
               <h1>Income status statistics</h1>
-              <h1>{IncomeAmount}</h1>
+              <h1> Total : {IncomeAmount}</h1>
             </div>
 
             <div className='col-span-full grid grid-cols-12 gap-x-[10px]'>
             {incomeStatusStats && Object.values(incomeStatusStats).map((item,index)=>{
                 return (
-                  <StatCard {...item} bgclass={"bg-green-300"} key={index} icon={<div>hello</div>}/>
+                  <StatCard {...item} bgclass={"bg-green-400"} key={index} icon={<div>hello</div>}/>
                 )
             })}
             </div>
@@ -55,22 +55,34 @@ const Stats = () => {
 
         <div className="expenese-stats grid grid-cols-12 gap-x-[10px]">
 
-            <div className='col-span-full flex justify-between px-4'>
+            <div className='col-span-full flex justify-between px-4 bg-purple-600 text-white my-4 py-2 rounded'>
               <h1>Expense category statistics</h1>
-              <h1>{ExpenseAmount}</h1>
+              <h1>Total : {ExpenseAmount}</h1>
             </div>
+
+            <div className='col-span-full grid grid-cols-12 gap-x-[10px]'>
             {expenseCategoryStats && Object.values(expenseCategoryStats).map((item,index)=>{
                 return (
-                  <StatCard {...item} bgclass={"bg-red-300"} key={index}/>
+                  <StatCard {...item} bgclass={"bg-red-400"} key={index}/>
                 )
             })}
-            <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+            </div>
 
+            <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700 col-span-full" />
+
+            
+            <div className='col-span-full flex justify-between px-4 bg-purple-600 text-white my-4 py-2 rounded'>
+              <h1>Expense status statistics</h1>
+              <h1>Total : {ExpenseAmount}</h1>
+            </div>
+
+            <div className='col-span-full grid grid-cols-12 gap-x-[10px]'>
             {expenseStatusStats && Object.values(expenseStatusStats).map((item,index)=>{
                 return (
                   <StatCard {...item} bgclass={"bg-red-400"} key={index}/>
                 )
             })}
+            </div>
 
         </div>
     </div>
