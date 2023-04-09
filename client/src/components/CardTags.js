c7de14b817a9b331e0de45bd555d938c1fc64f8b
Avijit Karmaker <ajkacca@gmail.com>
2023-04-09 08:07:51 +0300

    update cards

--- client/src/components/CardTags.js
+++ client/src/components/CardTags.js
@@ -3,13 +3,13 @@ import React from 'react'
 const CardTags = ({info:{cardClass,totalIncomes=null,totalExpenses=null}}) => {
   return (
     <div className={`${cardClass} grid grid-cols-12 text-white py-2`}>
-        <p className='col-span-2 place-self-center'>Transaction</p>
-        <p className='col-span-2 place-self-center'>Status</p>
-        <p className='col-span-2 place-self-center'>Information</p>
-        <p className='col-span-2 place-self-center'>Category</p>
-        <p className='col-span-2 place-self-center'>Amount</p>
-        {totalIncomes && <p className='col-span-2 place-self-center'>{totalIncomes} transaction found</p>}
-        {totalExpenses && <p className='col-span-2 place-self-center'>{totalExpenses} transaction found</p>}
+        <p className='hidden md:block md:col-span-2 place-self-center'>Transaction</p>
+        <p className='hidden md:block md:col-span-2 place-self-center'>Information</p>
+        <p className='hidden md:block md:col-span-2 place-self-center'>Status</p>
+        <p className='hidden md:block md:col-span-2 place-self-center'>Category</p>
+        <p className='hidden md:block md:col-span-2 place-self-center'>Amount</p>
+        {totalIncomes && <p className='col-span-full md:col-span-2 place-self-center'>{totalIncomes} transaction found</p>}
+        {totalExpenses && <p className='col-span-full md:col-span-2 place-self-center'>{totalExpenses} transaction found</p>}
     </div>
   )
 }
--- client/src/components/IncomeCard.js
+++ client/src/components/IncomeCard.js
@@ -8,11 +8,11 @@ const IncomeCard = ({title,amount,status,information,category,_id:id}) => {
 
   return (
     <div className='bg-white py-2 my-2 grid grid-cols-12'>
-        <h1 className='col-span-2 place-self-center'>{title}</h1>
-        <p className='col-span-2 place-self-center'>{status.toUpperCase()}</p>
-        <p className='col-span-2 place-self-center'>{information}</p>
-        <p className='col-span-2 place-self-center'>{category}</p>
-        <p className='col-span-2 place-self-center'>{amount}</p>
+        <h1 className='col-span-full md:col-span-2 place-self-start md:place-self-center'>{title}</h1>
+        <p className='col-span-full md:col-span-2 place-self-center'>{information}</p>
+        <p className='col-span-full md:col-span-2 place-self-start md:place-self-center'>{status.toUpperCase()}</p>
+        <p className='col-span-full md:col-span-2 place-self-center'>{category}</p>
+        <p className='col-span-full md:col-span-2 place-self-center'>{amount}</p>
         <NavLink to={`/dashboard/edit-income/${id}`} className='col-span-1 bg-orange-300 w-fit h-fit px-2 py-1 rounded' onClick={()=>setIncomeEdit(id)}>Edit</NavLink>
         <button className='col-span-1 bg-red-400 w-fit h-fit px-2 py-1 rounded text-white' onClick={()=>{deleteIncome(id)}}>Delete</button>
     </div>
