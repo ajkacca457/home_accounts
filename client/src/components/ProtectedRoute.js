a06f44e490151bb38632d6f3da3c1ac2dc8fa68f
Avijit Karmaker <ajkacca@gmail.com>
2023-04-09 04:15:24 +0300

    handled expired token

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -2,11 +2,24 @@ import React from 'react'
 import { useGlobalContext } from '../context/GlobalContext'
 import { Navigate } from 'react-router-dom';
 
+
+
 const ProtectedRoute = ({children}) => {
- const { user }= useGlobalContext();
+ const { user,token,logOutUser }= useGlobalContext();
  if(!user) {
    return <Navigate to="/auth" replace />;
  }
+
+
+ //tentative solution
+ 
+if(token) {
+  const {exp}=JSON.parse(atob(token.split(".")[1]));
+  if(exp*1000<Date.now()){
+    logOutUser();
+  }
+}
+
   return children
 }
 
