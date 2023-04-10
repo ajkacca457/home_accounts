b3470f2cc1e3eb79bf1bd6cf6fb72dd05f62850d
Avijit Karmaker <ajkacca@gmail.com>
2023-04-10 10:48:39 +0300

    add date to each transaction

--- client/src/components/ExpenseCard.js
+++ client/src/components/ExpenseCard.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { useGlobalContext } from '../context/GlobalContext';
 import { NavLink } from 'react-router-dom';
+import moment from 'moment';
 
-const ExpenseCard = ({title,amount,status,information,category,_id:id}) => {
+const ExpenseCard = ({title,amount,status,information,category,_id:id, createdAt}) => {
 
   const {deleteExpense, setExpenseEdit}= useGlobalContext();
 
+  let date= moment(createdAt);
+  date= date.format("MMM Do, YYYY");
+
   return (
-    <div className='bg-white px-2 py-2 my-2 grid grid-cols-12 gap-[10px]'>
+    <>
+    <div className='bg-white px-2 py-2 mt-3 grid grid-cols-12 gap-[10px]'>
         <h1 className='col-span-full md:col-span-2 place-self-start md:place-self-center underline md:no-underline'>{title}</h1>
         <p className='col-span-full md:col-span-2 place-self-start md:place-self-center'>{information}</p>
         <p className='col-span-4 md:col-span-2 place-self-start md:place-self-center'><span className='inline md:hidden'>Status:{" "}</span>{status.toUpperCase()}</p>
@@ -16,6 +21,8 @@ const ExpenseCard = ({title,amount,status,information,category,_id:id}) => {
         <NavLink to={`/dashboard/edit-expense/${id}`} className='col-span-6 md:col-span-1 bg-orange-300 w-full md:w-fit h-fit px-2 py-1 rounded text-center' onClick={()=>setExpenseEdit(id)}>Edit</NavLink>
         <button className='col-span-6 md:col-span-1 bg-red-400 w-full md:w-fit h-fit px-2 py-1 rounded text-white' onClick={()=>{deleteExpense(id)}}>Delete</button>
     </div>
+    <p className='bg-gray-300 text-right px-2'>Created At: {date}</p> 
+    </>
   )
 }
 
