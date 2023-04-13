997f65f091a33d2e1c6c3daab87f1fd066835b21
Avijit karmaker <ajkacca@gmail.com>
2023-04-13 16:14:32 +0300

    update profile

--- client/src/pages/UserDetail.js
+++ client/src/pages/UserDetail.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { useGlobalContext } from '../context/GlobalContext';
+import { FaUserCircle } from "react-icons/fa";
 const UserDetail = () => {
 
     const { user } = useGlobalContext();
+    const [isEdit, setIsEdit] = React.useState(false);
+
+    if (isEdit) {
+        return (<h1>Edit profile</h1>);
+    }
     return (
-        <div>
+        <div className='py-8'>
             {user &&
-                <div>
-                    <h1>{user.firstname}</h1>
-                    <h1>{user.lastname}</h1>
-                    <p>{user.email}</p>
-                    <p>{user.location}</p>
+                <div className='bg-white p-4 text-center rounded text-2xl'>
+                    <FaUserCircle className='text-6xl mx-auto my-4 text-blue-600' />
+                    <h1 className='my-2'>{user.firstname}{" "}{user.lastname}</h1>
+                    <p className='my-2'>{user.email}</p>
+                    <p className='my-2'>{user.location}</p>
+                    <button className='bg-blue-600 text-white px-3 py-1 rounded my-2' onClick={() => setIsEdit(true)}>Edit</button>
                 </div>}
         </div>
     );
--- client/src/utilites/linkData.js
+++ client/src/utilites/linkData.js
@@ -1,33 +1,40 @@
-import {FcStatistics, FcMoneyTransfer} from "react-icons/fc"
-import {TiShoppingCart} from "react-icons/ti"
-import {BsPlusSquare} from "react-icons/bs"
+import { FcStatistics, FcMoneyTransfer } from "react-icons/fc";
+import { TiShoppingCart } from "react-icons/ti";
+import { BsPlusSquare } from "react-icons/bs";
+import { FaUserCircle } from "react-icons/fa";
 
-export const links= [
+export const links = [
     {
-        id:1,
-        path:"/dashboard",
-        text:"Statistics",
-        icon:<FcStatistics/>
+        id: 1,
+        path: "/dashboard",
+        text: "Statistics",
+        icon: <FcStatistics />
     },
     {
-        id:2,
-        path:"/dashboard/incomes",
-        text:"All Incomes",
-        icon:<FcMoneyTransfer/>
+        id: 2,
+        path: "/dashboard/incomes",
+        text: "All Incomes",
+        icon: <FcMoneyTransfer />
     },
     {
-        id:3,
-        path:"/dashboard/expenses",
-        text:"All Expenses",
-        icon:<TiShoppingCart/>
+        id: 3,
+        path: "/dashboard/expenses",
+        text: "All Expenses",
+        icon: <TiShoppingCart />
     },
     {
-        id:4,
-        path:"/dashboard/add-transaction",
-        text:"Add Transaction",
-        icon:<BsPlusSquare/>
+        id: 4,
+        path: "/dashboard/add-transaction",
+        text: "Add Transaction",
+        icon: <BsPlusSquare />
     },
-]
+    {
+        id: 5,
+        path: "/dashboard/user",
+        text: "Profile",
+        icon: <FaUserCircle />
+    },
+];
 
-export const incomeCategories=["Salary","Profit", "Investment", "Interest", "Other"];
-export const expenseCategories=["Groceries","Rent", "Insurance", "Entertainment", "Education","Health","Miscellaneous"];
+export const incomeCategories = ["Salary", "Profit", "Investment", "Interest", "Other"];
+export const expenseCategories = ["Groceries", "Rent", "Insurance", "Entertainment", "Education", "Health", "Miscellaneous"];
