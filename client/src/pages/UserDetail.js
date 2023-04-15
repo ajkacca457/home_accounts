c0e9c89fe2b08d06c3eed9d5476ed0a89521f437
Avijit Karmaker <ajkacca@gmail.com>
2023-04-15 11:05:14 +0300

    update profile form

--- client/src/pages/UserDetail.js
+++ client/src/pages/UserDetail.js
@@ -1,13 +1,35 @@
 import React, { useState } from 'react';
 import { useGlobalContext } from '../context/GlobalContext';
 import { FaUserCircle } from "react-icons/fa";
+import FormRow from '../components/FormRow';
+import {MdArrowBack} from "react-icons/md";
 const UserDetail = () => {
 
-    const { user } = useGlobalContext();
+    const { user, isLoading } = useGlobalContext();
     const [isEdit, setIsEdit] = useState(false);
+    const [updateUser,setUpdateUser]= useState(user);
+
+    const handleChange=(e)=>{
+        setUpdateUser({...updateUser,[e.target.name]:e.target.value});
+    }
+
+    const userUpdate=(e)=>{
+        e.preventDefault();
+        console.log(updateUser);
+    }
+
 
     if (isEdit) {
-        return (<h1>Edit profile</h1>);
+        return (    
+        <form className='w-full md:w-10/12 lg:w-9/12 mx-auto bg-white mt-8 rounded p-6' onSubmit={userUpdate}>
+            <h1 className='bg-blue-600 text-center my-2 rounded text-white py-2'>Edit profile</h1>
+            <FormRow type="text" name="firstname" value={updateUser.firstname} labelText="Firstname" placeholderText="Enter your first name" handleChange={handleChange}/>
+            <FormRow type="text" name="lastname" value={updateUser.lastname} labelText="Lastname" placeholderText="Enter your last name" handleChange={handleChange}/>
+            <FormRow type="email" name="email" value={updateUser.email} labelText="Email" placeholderText="Enter email address" handleChange={handleChange}/>
+            <FormRow type="text" name="location" value={updateUser.location} labelText="Lastname" placeholderText="Enter your location" handleChange={handleChange}/>
+            <button className="btn-primary disabled:opacity-75 disabled:bg-slate-400 my-6" type="submit" disabled={isLoading} >Submit</button>
+            <button className='btn-underline flex items-center gap-x-[5px]' onClick={(e)=>{setIsEdit(!isEdit)}}><MdArrowBack/> Back to profile</button>
+        </form>  );
     }
     return (
         <div className='py-8'>
@@ -17,7 +39,7 @@ const UserDetail = () => {
                     <h1 className='my-2'>{user.firstname}{" "}{user.lastname}</h1>
                     <p className='my-2'>{user.email}</p>
                     <p className='my-2'>{user.location}</p>
-                    <button className='bg-gray-600 hover:bg-blue-300 text-white px-3 py-1 rounded my-2' onClick={() => setIsEdit(true)}>Edit</button>
+                    <button className='bg-gray-600 hover:bg-blue-300 text-white px-3 py-1 rounded my-2' onClick={() => setIsEdit(!isEdit)}>Edit</button>
                 </div>}
         </div>
     );
