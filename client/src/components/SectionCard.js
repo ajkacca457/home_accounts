f290f4f84729e0bd7d2b410f39e7743ca5c25129
Avijit karmaker <ajkacca@gmail.com>
2023-04-19 12:53:09 +0300

    update homepage style

--- client/src/components/SectionCard.js
+++ client/src/components/SectionCard.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-const SectionCard = ({cardImage,left,info,title}) => {
+const SectionCard = ({ cardImage, left, info, title }) => {
 
-  return (
-    <div className="image-card grid grid-cols-12 gap-x-4 items-center w-5/6 mx-auto my-16">
-        <div className={`col-span-6 ${left?"order-2":""}`}>
-            <h1 className='font-heading'>{title.toUpperCase()}</h1>
-            <p>{info}</p>
-        </div>
-        <img src={cardImage} alt="" className='col-span-6 rounded-md'/>
-    </div>  )
-}
+    return (
+        <div className="image-card grid grid-cols-12 gap-x-4 items-center w-5/6 mx-auto my-16">
+            <div className={`col-span-6 ${left ? "order-2" : ""}`}>
+                <h1 className='font-heading text-xl'>{title.toUpperCase()}</h1>
+                <p className='text-lg'>{info}</p>
+            </div>
+            <img src={cardImage} alt="" className='col-span-6 rounded-md' />
+        </div>);
+};
 
-export default SectionCard
+export default SectionCard;
--- client/src/utilites/cardData.js
+++ client/src/utilites/cardData.js
@@ -3,15 +3,15 @@ import Saving from "../assets/saving.jpg";
 
 export const cardDetails = [
     {
-    cardImage:Finace,
-    title:"Track your finances easily",
-    left:false,
-    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi dolor unde suscipit tempore eveniet maxime! Asperiores eligendi molestias sit libero?"
+        cardImage: Finace,
+        title: "Track your finances easily",
+        left: false,
+        info: "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver. So drive safe and track your finances easily"
     },
     {
-    cardImage:Saving,
-    title:"Be confiedent in case of saving your own money",
-    left:true,
-    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi dolor unde suscipit tempore eveniet maxime! Asperiores eligendi molestias sit libero?"
+        cardImage: Saving,
+        title: "Be confiedent in case of saving your own money",
+        left: true,
+        info: "Many people take no care of their money till they come nearly to the end of it, and others do just the same with their time."
     }
-]
+];
